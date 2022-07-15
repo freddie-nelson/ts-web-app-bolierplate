553d3c91efdc9f21920eca8e0031b4e3bab490f7
Freddie Nelson <freddie0208@hotmail.com>
2022-07-15 20:16:37 +0100

    init

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app");
+if (app)
+  app.innerHTML = `
+  <h1 class="text-5xl font-bold">Vite + TS + Tailwind + ESLint + Prettier + TS-Jest</h1> 
+`;
