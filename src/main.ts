f75833f11e144d743d8627338a3160e7b8588b46
Freddie Nelson <freddie0208@hotmail.com>
2022-07-16 20:15:33 +0100

    add sass and @ src alias

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import "./style.css";
+import "@/style.scss";
 
 const app = document.querySelector<HTMLDivElement>("#app");
 if (app)
   app.innerHTML = `
-  <h1 class="text-5xl font-bold">Vite + TS + Tailwind + ESLint + Prettier + TS-Jest</h1> 
+  <h1 class="text-5xl font-bold">Vite + TS + SASS + Tailwind + ESLint + Prettier + TS-Jest</h1> 
 `;
